f931d4a9c59b07cb1acff901117255422e0406df
jamesoneill997 <jamesoneill997@gmail.com>
2020-04-10 18:30:37 +0100

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -21,7 +21,7 @@ try {
             messageThree.textContent = ''
             }
 
-        fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+        fetch('/weather?address='+location).then((response)=>{
         response.json().then((data)=>{
             
             if(data.error){
--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 
 //define paths for express config
@@ -92,6 +93,6 @@ app.get('*', (req,res)=>{
     })
 })
 
-app.listen(3000, ()=>{
-    console.log('Server running')
+app.listen(port, ()=>{
+    console.log('Server running on port '+port)
 })
