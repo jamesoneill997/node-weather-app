e79f6aa329df888a2d23269739035a77971dc669
jamesoneill997 <jamesoneill997@gmail.com>
2020-04-11 02:13:55 +0100

    updated design, centered content

---
+++ public/img/My weather service_files/app.js
@@ -0,0 +1,48 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
+try {
+   
+        const data = weatherForm.addEventListener('submit',(e)=>{
+
+    
+        event.preventDefault()
+        const location = search.value
+
+        messageOne.textContent = 'Loading...'
+        messageTwo.textContent = ''
+        messageThree.textContent = ''
+
+        if (location.includes(';')){
+            messageOne.textContent = 'Place does not exist. Please check spelling and try again.'
+            messageTwo.textContent = ''
+            messageThree.textContent = ''
+            }
+
+        fetch('/weather?address='+location).then((response)=>{
+        response.json().then((data)=>{
+            
+            if(data.error){
+                messageOne.textContent = data.error
+                messageTwo.textContent = ''
+                messageThree.textContent = ''
+            }
+            else{
+                messageOne.textContent = 'Location: ' + data.address + ' || Local time: ' + data.time
+                messageTwo.textContent =  'Temperature: ' + data.temp
+                messageThree.textContent = 'Conditions: '+ data.conditions
+                console.log(data)
+            }
+        })
+    })
+})
+}
+catch (TypeError) {
+        messageOne.textContent = 'Place does not exist. Please check spelling and try again.'
+        messageTwo.textContent = ''
+        messageThree.textContent = ''
+
+
+}
