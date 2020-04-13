e041db291c78bf95bc99d5035e2ead3075aec210
jamesoneill997 <jamesoneill997@gmail.com>
2020-04-13 01:33:30 +0100

    added condition-specific image functionality

--- public/js/app.js
+++ public/js/app.js
@@ -10,15 +10,18 @@ try {
     
         event.preventDefault()
         const location = search.value
+        var resultImage = document.getElementById("condition-image")
 
         messageOne.textContent = 'Loading...'
         messageTwo.textContent = ''
         messageThree.textContent = ''
+        resultImage.innerHTML = ''
 
         if (location.includes(';')){
             messageOne.textContent = 'Place does not exist. Please check spelling and try again.'
             messageTwo.textContent = ''
             messageThree.textContent = ''
+            resultImage.innerHTML = ''
             }
 
         fetch('/weather?address='+location).then((response)=>{
@@ -28,11 +31,39 @@ try {
                 messageOne.textContent = data.error
                 messageTwo.textContent = ''
                 messageThree.textContent = ''
+                resultImage.innerHTML = ''
             }
             else{
                 messageOne.textContent = 'Location: ' + data.address + ' || Local time: ' + data.time
                 messageTwo.textContent =  'Temperature: ' + data.temp
                 messageThree.textContent = 'Conditions: '+ data.conditions
+                if (data.conditions === 'Clear') {
+                    resultImage.innerHTML = '<img id="weather-image" src="/img/clear.jpg" alt="Clear Image">'
+
+                } else if(data.conditions === 'Sunny') {
+                    resultImage.innerHTML = '<img id="weather-image" src="/img/sunny.jpg" alt="Other Image">'
+                }
+                
+                else if(data.conditions === 'Hazy') {
+                    resultImage.innerHTML = '<img id="weather-image" src="/img/hazy.jpg" alt="Other Image">'
+                }
+                
+                else if(data.conditions === 'Rain') {
+                    resultImage.innerHTML = '<img id="weather-image" src="/img/rainy.jpg" alt="Other Image">'
+                }
+
+                else if(data.conditions === 'Partly cloudy') {
+                    resultImage.innerHTML = '<img id="weather-image" src="/img/partly-cloudy.jpeg" alt="Other Image">'
+                }
+                else if(data.conditions === 'Thundery outbreaks possible') {
+                    resultImage.innerHTML = '<img id="weather-image" src="/img/thunder.jpg" alt="Other Image">'
+                }
+                
+                else{
+                    resultImage.innerHTML = ''
+                }
+                
+
                 console.log(data)
             }
         })
